62ddfc4bb54f3e2e75a158dc84b01b73615ac47a
Michael Siegle <siegle-michael@web.de>
2023-07-14 23:15:43 +0200

    do not use static locahost for requests

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -186,7 +186,7 @@ export default defineComponent({
         });
         return;
       }
-      fetch(`http://localhost:${this.port}/fetch`, {
+      fetch(`${location.host}:${this.port}/fetch`, {
         method: 'POST',
         headers: {
           'Access-Control-Allow-Origin': '*', // Required for CORS support to work,
